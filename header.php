b72e5205ad933ae00dddf339c96b44de39b75922
Rutger Vos <99033234@mydavinci.nl>
2020-03-10 13:27:56 +0100

    10-3-2020 basis gemaakt

---
+++ header.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      to-do-list
+    </span>
+  </div>
+</nav>
+<?php ?>
---
+++ home.php
@@ -0,0 +1,22 @@
+<?php include 'header.php' ?>
+<?php include 'footer.php' ?>
+
+<html>
+<button type="button" class="btn btn-primary btn-lg btn-block">create to-do-list</button>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">edit</a>
+    <a href="#" class="card-link">delete all</a>
+    <a href="#" class="card-link">delete on task</a>
+  </div>
+</div>
+</html>
