4885c89572b07a5aa2143594a87e8ab4b1a4642e
Rutger Vos <99033234@mydavinci.nl>
2020-03-13 13:26:14 +0100

    13-2-2020 added more files and made begin on creating of lists

--- createlist.php
+++ createlist.php
@@ -6,7 +6,7 @@ $password = "mysql";
 $myDB = "todolist";
 
 header('Content-type: text/html; charset=iso-8859-1');
-
+$game = getlistsById($_GET['id'],$conn);
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
     // set the PDO error mode to exception
@@ -19,19 +19,25 @@ catch(PDOException $e)
     echo "Connection failed: " . $e->getMessage();
     }
    
-$stmt = $conn->prepare("INSERT INTO `lists` (listname, tasks) 
-    VALUES (:listname , :tasks)");
-    $stmt->bindParam(':listname', );
-    $stmt->bindParam(':tasks', );
-    $stmt->execute();
+//$stmt = $conn->prepare("INSERT INTO `lists` (listname, tasks) 
+   // VALUES (:listname , :tasks)");
+    //$stmt->bindParam(':listname', $_POST['listname']);
+    //$stmt->bindParam(':tasks', $_POST['tasks']);
+    //$stmt->bindParam(':tasks', $_POST['']);
+    //$stmt->execute();
+
 ?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?id=<?= $game['id'] ?>" method="POST">
+<form action="" method="POST">
 <!-- <form action="add.php?id=<?php //echo $game['id'] ?>" method="POST"> -->
 <div class="input-group mb-3">
   <div class="input-group-prepend">
-    <span class="input-group-text" id="inputGroup-sizing-default"i>name to do list</span>
+    <span class="input-group-text" id="inputGroup-sizing-default">name to do list</span>
   </div>
   <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" name="listname" value="">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">tasks</span>
+  </div>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" name="tasks" value="">
 </div>
   <input type="submit" value="Submit" onclick="">
 </form>
--- header.php
+++ header.php
@@ -9,7 +9,7 @@
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="home.php">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#"></a>
